fbbe9ed91d3552263bb4cc56c47ecd98a8e1f7de
Govendra Sahu <72web.developer@gmail.com>
2024-01-21 19:54:52 +0530

    swiper added

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
     },
     pagination: {
